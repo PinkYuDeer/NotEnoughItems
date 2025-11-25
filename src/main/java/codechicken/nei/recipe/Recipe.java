126f0ea73cd6d1941a61f02c2c197d5fbbaa855b
slprime <31038811+slprime@users.noreply.github.com>
2025-11-25 08:24:30 +0200

    Fix missing favorites and incorrect crafting chains for recipes with identical ingredients (#814)

--- src/main/java/codechicken/nei/recipe/Recipe.java
+++ src/main/java/codechicken/nei/recipe/Recipe.java
@@ -236,6 +236,12 @@ public class Recipe {
                     return false;
                 }
 
+                if (isShapedRecipe() && this.result != null
+                        && anRecipeId.result != null
+                        && !StackInfo.equalItemAndNBT(this.result, anRecipeId.result, true)) {
+                    return false;
+                }
+
                 for (int idx = 0; idx < this.ingredients.size(); idx++) {
                     if (!StackInfo.equalItemAndNBT(this.ingredients.get(idx), anRecipeId.ingredients.get(idx), true)) {
                         return false;
